0d6aebf57fa48a4cdee40961cfa4e3a89d0771c3
alloy <onedayaslion@gmail.com>
2022-09-19 23:15:40 +0500

    add task1.py

---
+++ lesson6/task1.py
@@ -0,0 +1,10 @@
+transformation = lambda x: x
+
+values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+transformed_values = list(map(transformation, values))
+
+if values == transformed_values:
+    print("ok")
+else:
+    print("false")
